2a2984f4a7cf98f394f0989f7ad482526aa215f3
Cellul4r <81496761+Cellul4r@users.noreply.github.com>
2024-09-01 10:09:22 +0700

    Create tarjanAlgo.cpp

---
+++ Graph/tarjanAlgo.cpp
@@ -0,0 +1,30 @@
+// adjacency list
+vector<int> G[N];
+
+bool visited[N];
+int disc[N], low[N];
+
+set<int> ap; // answer: articulation points
+set<pii> bridge; // answer: bridges
+int counter = 0;
+void tarjan(int u, int p) { // p = parent of u
+    visited[u] = true;
+    low[u] = disc[u] = ++counter;
+    int child = 0;
+    for (auto v : G[u]) {
+        if (!visited[v]) {
+            ++child;
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+            // articulation point
+            // parent of root is 0.
+            if ((p != 0 && low[v] >= disc[u]) || (p == 0 && child > 1))
+                ap.insert(u);
+            // bridge
+            if (low[v] > disc[u])
+                bridge.insert(pii(u, v));
+        } else if (v != p) {
+            low[u] = min(low[u], disc[v]);
+        }
+    }
+}
