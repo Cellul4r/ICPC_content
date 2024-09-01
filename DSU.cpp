bd86ef2b23f291ab6b2f078a2a87db95e09e1f21
Cellul4r <81496761+Cellul4r@users.noreply.github.com>
2024-09-01 09:07:37 +0700

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,11 @@
+//DSU
+ll parent[NN], sz[NN];
+ll find(ll a){ return a == parent[a] ? a : parent[a] = find(parent[a]); }
+void merge(ll u, ll v) {
+    u = find(u), v=find(v);
+    if (u!=v) {
+        if (sz[u]<sz[v]) swap(u, v);
+        sz[u] += sz[v];
+        parent[v] = u;
+    }
+}
