08624bf3d8cbc0f77db53c2d9305eafe01e718f2
Cellul4r <81496761+Cellul4r@users.noreply.github.com>
2024-09-01 09:35:34 +0700

    Create DigitDP.cpp

---
+++ DigitDP.cpp
@@ -0,0 +1,13 @@
+#define DP dp[pos][is_eq]
+ll solve(int pos, bool is_eq) {
+	if (~DP) return DP;
+	if (pos==n)
+		//check for predicate (here it is p(x)=True)
+		return DP=1;
+
+	DP = 0;
+	for (int i=0;i<=(is_eq?r[pos]:9);i++)
+		DP += solve(pos+1, is_eq && i==r[pos]);
+
+	return DP;
+}
